0067e5c9f0d3c23b4b73b37268997d89c1497f8a
Danut Popa <scorpionipx@gmail.com>
2019-09-28 19:26:23 +0300

    Create global.h

---
+++ Atmega32A16x2LCD/Atmega32A16x2LCD/global.h
@@ -0,0 +1,16 @@
+/*
+ * global.h
+ *
+ * Created: 28-Sep-19 19:04:48
+ *  Author: ScorpionIPX
+ */ 
+
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#define F_CPU 16000000UL
+
+
+
+#endif /* GLOBAL_H_ */
